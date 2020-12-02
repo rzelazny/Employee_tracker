b0d09b755ebc92d999b27f7f0a274163c72bbb48
Ryan Zelazny <RyanZelazny@gmail.com>
2020-12-01 19:48:04 -0500

    created schema

---
+++ schema.sql
@@ -0,0 +1,30 @@
+drop database if exists company_db;
+create database company_db;
+
+use company_db;
+
+create table department(
+    id int auto_increment not null,
+    dept_name varchar(30),
+    primary key (id)
+);
+
+create table role (
+    id int auto_increment not null,
+    title varchar(30),
+    salary decimal (10,2),
+    department_id int,
+    primary key (id),
+    foreign key (department_id) references department(id)
+);
+
+create table employee (
+    id int auto_increment not null,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int,
+    manager_id int,
+    primary key (id),
+    foreign key (role_id) references role(id),
+    foreign key (manager_id) references employee(id)
+);
