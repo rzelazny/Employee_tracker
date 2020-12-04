a002834785d73b75391dcfdaf740a1a3cab3ed16
Ryan Zelazny <RyanZelazny@gmail.com>
2020-12-03 19:37:32 -0500

    adding helper functions

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,42 @@
 var connection = require("./connection.js");
 
+// Helper functions for SQL syntax, adds necessary number of '?' marks.
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+            value = "'" + value + "'";
+            }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+//The object relational mapping functions
 var orm = {
-    // The last variable cb represents the anonymous function being passed from server.js
+    // The last variable cb represents the anonymous callback function
     selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM ??";
     connection.query(queryString, [tableInput], function(err, result) {
@@ -27,7 +62,7 @@ var orm = {
             cb(result);
         });
     },
-    
+
     create: function(table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
     
